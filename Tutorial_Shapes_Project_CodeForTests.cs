3e52f96867d3f6b0e73ebf32550c9306e1a397dd
sudeoztas <s26727@pjwstk.edu.pl>
2025-11-07 15:16:27 +0100

    Initial commit - Assignment 3 Basic Unit Test

---
+++ Tutorial_Shapes_Project_CodeForTests.cs
@@ -0,0 +1,24 @@
+using Tut2_s20123;
+
+namespace Tut2_s20123_Tests
+{
+    public class Tests
+    {
+        private readonly IShape sphere = new Sphere(5);
+
+        [Test]
+        public void TestSphereCalculateArea()
+        {
+            Assert.That(sphere.CalculateArea(), Is.EqualTo(314.159).Within(0.001));
+        }
+
+        [Test]
+        public void TestSphereCalculateVolume()
+        {
+            Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
+        }
+
+        //todo : Complete the remaning tests here
+
+    }
+}

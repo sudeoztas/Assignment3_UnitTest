3b8be86e4db7010ff86d57590bb5b25c1aa2fb9d
sudeoztas <s26727@pjwstk.edu.pl>
2026-01-02 20:01:23 +0100

    Add tests for Cylinder, Rectangle, and Cube shapes

--- Tutorial_Shapes_Project_CodeForTests.cs
+++ Tutorial_Shapes_Project_CodeForTests.cs
@@ -5,6 +5,11 @@ namespace Tut2_s20123_Tests
     public class Tests
     {
         private readonly IShape sphere = new Sphere(5);
+        private readonly IShape cylinder = new Cylinder(3, 7);
+        private readonly IShape rectangle = new Rectangle(4, 8);
+        private readonly IShape cube = new Cube(4);
+
+        // -------- Sphere Tests --------
 
         [Test]
         public void TestSphereCalculateArea()
@@ -18,7 +23,46 @@ namespace Tut2_s20123_Tests
             Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
         }
 
-        //todo : Complete the remaning tests here
+        // -------- Cylinder Tests --------
 
+        [Test]
+        public void TestCylinderCalculateArea()
+        {
+            Assert.That(cylinder.CalculateArea(), Is.EqualTo(188.496).Within(0.001));
+        }
+
+        [Test]
+        public void TestCylinderCalculateVolume()
+        {
+            Assert.That(cylinder.CalculateVolume(), Is.EqualTo(197.920).Within(0.001));
+        }
+
+        // -------- Rectangle Tests --------
+
+        [Test]
+        public void TestRectangleCalculateArea()
+        {
+            Assert.That(rectangle.CalculateArea(), Is.EqualTo(32));
+        }
+
+        [Test]
+        public void TestRectangleCalculateVolume()
+        {
+            Assert.That(rectangle.CalculateVolume(), Is.EqualTo(0));
+        }
+
+        // -------- Cube Tests --------
+
+        [Test]
+        public void TestCubeCalculateArea()
+        {
+            Assert.That(cube.CalculateArea(), Is.EqualTo(96));
+        }
+
+        [Test]
+        public void TestCubeCalculateVolume()
+        {
+            Assert.That(cube.CalculateVolume(), Is.EqualTo(64));
+        }
     }
 }
